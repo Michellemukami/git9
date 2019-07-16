c12ac12c0b48f5cc1f852cad0986909a1aef32ad
Michelle Mukami <you@example.com>
2019-07-16 11:34:53 +0300

    Push to ghpages.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl:'api.github.com/users/',
-  access_token:'?access_token=8b95c9c8bdfb194bb5efa4582e40c05c6fe11377'
+  access_token:'b372f23587fb5572e8fd7bfca3502662af1628b'
 };
